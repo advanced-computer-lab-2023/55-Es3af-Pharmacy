f9cc585c711605f11ed4b2469b961345277f1381
farahdawod <farahdawod@outlook.com>
2023-10-08 19:51:46 +0300

    medicine functions transferred to pharmacist route

--- src/routes/medicine.routes.js
+++ src/routes/medicine.routes.js
@@ -3,13 +3,6 @@ const medicinController = require('../controllers/medicine.controller')
 
 const medicineRoutes = new Router()
 
-medicineRoutes.post("/addMedicine", medicinController.addMedicine);
-
 medicineRoutes.get("/medicine", medicinController.getMedicine);
-
-medicineRoutes.put("/updateMedicine", medicinController.updateMedicine);
-
-medicineRoutes.delete("/deleteMedicine", medicinController.deleteMedicine);
-
 
 module.exports = {medicineRoutes};
--- src/routes/pharmacist.routes.js
+++ src/routes/pharmacist.routes.js
@@ -1,10 +1,19 @@
 const { Router } = require('express')
 const pharmacistController = require('../controllers/pharmacist.controller')
+const medicineController = require('../controllers/medicine.controller')
 
 const pharmacistRoutes = new Router()
 
 
-pharmacistRoutes.get('/:id', pharmacistController.getPharmacist)
+pharmacistRoutes.get('/:id', pharmacistController.getPharmacist);
+
+pharmacistRoutes.post("/addMedicine", medicineController.addMedicine);
+
+pharmacistRoutes.get("/medicine", medicineController.getMedicine);
+
+pharmacistRoutes.put("/updateMedicine", medicineController.updateMedicine);
+
+pharmacistRoutes.delete("/deleteMedicine", medicineController.deleteMedicine);
 
 
 module.exports = { pharmacistRoutes };
