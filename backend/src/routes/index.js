682dc56dcaa659c8265ac010a92a4339127faf9e
Ebadajr <ebada.jr@gmail.com>
2023-10-10 23:50:31 +0300

    requests

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ const { medicineRoutes } = require("./medicine.routes");
 const { userRoutes } = require("./user.routes");
 const { patientRoutes } = require("./patient.routes");
 const { pharmacistRoutes } = require("./pharmacist.routes");
+const { pharmacistRequestsRoutes } = require("./pharmacistRequests.routes");
 
 const router = new Router();
 
@@ -14,4 +15,7 @@ router.use("/patient", patientRoutes);
 
 router.use("/pharmacist", pharmacistRoutes);
 
+router.use("/pharmacist-requests",pharmacistRequestsRoutes )
+
 module.exports = { router };
+
